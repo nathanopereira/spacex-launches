1c0ff2b2fd457249d27313fac15fbcc5342cd876
Nathan Souza <nathanpereirasouza@gmail.com>
2021-05-30 17:13:32 -0300

    alterando rota para carregar próximos lançamentos ordenado

--- src/pages/api/launches/upcoming.ts
+++ src/pages/api/launches/upcoming.ts
@@ -2,7 +2,15 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import api from '../../../config/spacexApi'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { data } = await api.get('/launches/upcoming')
+  const { data } = await api.post('/launches/query', {
+    query: {
+      upcoming: true
+    },
+    options: {
+      sort: { date_utc: -1 },
+      pagination: false
+    }
+  })
 
   return res.json(data);
 }
