2cf10e5c2e7b5dd0332dd1a4ca4e6cde99a2303d
Nathan Souza <nathanpereirasouza@gmail.com>
2021-05-30 18:33:47 -0300

    adicionando script do Hotjar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -78,6 +78,19 @@ export default function Home() {
       <Head>
         <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
         <title>SpaceX Launches | by @nathanopereira</title>
+        {process.env.NODE_ENV === 'production' && (
+          // <!-- Hotjar Tracking Code for https://spacex-launches-nathan.vercel.app/ -->
+          <script dangerouslySetInnerHTML={{ __html: `
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:2428418,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}}/>
+        )}
       </Head>
 
       <div className="row">
