9f8966470bc211e61593a18b454c911aefd721d6
Nathan Souza <nathanpereirasouza@gmail.com>
2021-05-30 17:59:47 -0300

    adicionando estratégia inicial de cache na para a rota de foguetes

--- src/pages/api/rockets/[id].ts
+++ src/pages/api/rockets/[id].ts
@@ -1,8 +1,26 @@
+import { isBefore, subHours } from 'date-fns';
 import { NextApiRequest, NextApiResponse } from 'next';
 import api from '../../../config/spacexApi'
 
+// TODO move cache for Redis
+const cache = {
+  rockets: [],
+  lastUpdate: new Date()
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if(isBefore(cache.lastUpdate, subHours(new Date(), 12))){
+    cache.lastUpdate = new Date()
+    cache.rockets = []
+  }
+
+  const findRocket = cache.rockets.find(r => r.id === req.query.id)
+  if(findRocket){
+    return res.json(findRocket)
+  }
+
   const { data } = await api.get(`/rockets/${req.query.id}`)
+  cache.rockets.push(data);
 
   return res.json(data);
 }
